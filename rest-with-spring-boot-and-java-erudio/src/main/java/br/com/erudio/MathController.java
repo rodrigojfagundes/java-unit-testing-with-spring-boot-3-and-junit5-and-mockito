639169a50ac5be4b00565964cb57a81d9fb1f455
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-03-30 21:30:57 -0300

    Definindo a Logica da Nossa Aplicação
    S09A_FirstStepsInJavawithSpringBoot

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -7,11 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathController {
-	
-    
+
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne") String numberOne, 
-                      @PathVariable("numberTwo") String numberTwo) {
-        return 1d;
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo)
+
+                    		  throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }    
+    
+    public static Double covertToDouble(String strNumber) {
+        return 1.0d;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        return false;
     }
 }
